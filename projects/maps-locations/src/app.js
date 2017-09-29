50ebdc2cf9a628e056e708b466fcfee79e37ad2b
Bill Deng <billdevcode@gmail.com>
2017-09-28 20:46:17 -0700

    Add cache, Velocity library & center marker on mouseover

--- projects/maps-locations/src/app.js
+++ projects/maps-locations/src/app.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
+  
   var map, infowindow, querySearch, address, 
     useSideSearch = false, pyrmont, mapZoom = 14,
-    places = {};
+    places = {}, cachedPlaces = {};
   
   getGeoLocation();
 
   function getGeoLocation() {
-    var url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAytr024G7F4gULgAW1eMTtExZjaFwJuzE';
+    const url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAytr024G7F4gULgAW1eMTtExZjaFwJuzE';
     $.ajax({
-      url: url,
+      url,
       method: 'POST',
-    }).done(function(data) {
-      initMap(data.location.lat, data.location.lng);
-    }).fail(function() {
+    }).done(response => {
+      const { lat, lng } = response.location;
+      initMap(lat, lng);
+    }).fail(error => {
       alert("Cannot detect your location");
     });
   }
@@ -24,20 +26,18 @@ $(document).ready(function() {
       zoom: mapZoom,
     });
 
-    var defaultBounds = new google.maps.LatLngBounds(
+    const defaultBounds = new google.maps.LatLngBounds(
       new google.maps.LatLng(lat, lng),
       new google.maps.LatLng(lat, lng));
-    var input = document.getElementById('search-query');
-    var options = {
+    const input = document.getElementById('search-query');
+    const options = {
       bounds: defaultBounds,
     };
     autocomplete = new google.maps.places.Autocomplete(input, options);
   }
 
   $('.main-button, .sidenav-button').on("click", function() {
-    if ($("#search-query").val() === '') {
-      return;
-    } else if (useSideSearch && $("#sidenav-query").val() === '') {
+    if ($("#search-query").val() === '' || (useSideSearch && $("#sidenav-query").val() === '')) {
       return;
     }
 
@@ -54,60 +54,87 @@ $(document).ready(function() {
       zoom: mapZoom,
     });
 
-    var request = {
+    const request = {
       location: pyrmont,
       radius: '10',
       query: querySearch,
     };
 
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, callback);
+    if (cachedPlaces[querySearch]) {
+      cachePlaces();
+    } else {
+      infowindow = new google.maps.InfoWindow();
+      const service = new google.maps.places.PlacesService(map);
+      service.textSearch(request, callback);
+    }
+
+    function cachePlaces() {
+      const foundPlaces = cachedPlaces[querySearch];
+      const placesLength = foundPlaces.length;
+      places = {};
+      for (let i = 0; i < placesLength; i++) {
+        const { place_id } = foundPlaces[i];
+        createMarker(foundPlaces[i]);
+        addSearchResultToSide(foundPlaces[i]);
+        places[place_id] = foundPlaces[i];        
+      }
+    }
 
     function callback(results, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
+        const listOfPlacesForCache = [];        
+        const resultsLength = results.length;
+        for (let i = 0; i < resultsLength; i++) {
+          const { place_id } = results[i];
           createMarker(results[i]);
           addSearchResultToSide(results[i]);
-          places[results[i].place_id] = results[i];
+          places[place_id] = results[i];
+          listOfPlacesForCache.push(results[i]);
         }
+        cachedPlaces[querySearch] = listOfPlacesForCache;
       }
     }
 
     function addSearchResultToSide(place) {
-      address = place.formatted_address.split(',');
+      const { formatted_address, place_id, name } = place;
+      address = formatted_address.split(',');
       $(".results-list").append(
-        `<li id="${place.place_id}">${place.name}<br>
-        ${address[0]}<br>
-        ${address[1]}, ${address[2]}</li>`)
-    }
-    slideSideNav();
+        `<li id="${place_id}">
+          <p>${name}</p>
+          <p>${address[0]}</p>
+          <p>${address[1]}, ${address[2]}</p>
+        </li>`)
+    
+        addEscKeyListener();
+      }
+      slideSideNav();
 
     $(".results-list").on("mouseenter", "li", function() {
       var place;
-      var findPlace = $(this).attr("id");
+      const findPlace = $(this).attr("id");
 
       for (var id in places) {
         if (places[id].place_id === findPlace) {
           place = places[id];
         }
       }
-      var marker = new google.maps.Marker({
+      const marker = new google.maps.Marker({
         position: place.geometry.location,
-        map: map,
+        map
       })
       setContentToMarker(place);
       infowindow.open(map, marker);
       createMarker(place);
+      map.setCenter(marker.getPosition());
     });
 
     function createMarker(place) {
-      var marker = new google.maps.Marker({
-        map: map,
+      const marker = new google.maps.Marker({
+        map,
         position: place.geometry.location,
       });
 
-      google.maps.event.addListener(marker, 'click', function() {
+      google.maps.event.addListener(marker, 'mouseover', function() {
         setContentToMarker(place);
         infowindow.open(map, this);
       });
@@ -115,47 +142,46 @@ $(document).ready(function() {
   });
 
   function setContentToMarker(place) {
-    address = place.formatted_address.split(',');
+    const { formatted_address, name } = place;
+    address = formatted_address.split(',');
     infowindow.setContent(
-      `<strong>${place.name}</strong><br> 
-      ${address[0]}<br>
-      ${address[1]}, ${address[2]}`);
+      `<div class="mapMarker">
+        <h3>${name}</h3>
+        <p>${address[0]}</p>
+        <p>${address[1]}, ${address[2]}</p>
+      </div>`);
   }
 
   $("#search-query").keydown(function(e) {
-    if (e.which == 13) {
-      $(".main-button").click();
-    }
+    if (e.which == 13) { $(".main-button").click(); }
   });
 
   $("#sidenav-query").keydown(function(e) {
-    if (e.which == 13) {
-      $(".sidenav-button").click();
-    }
+    if (e.which == 13) { $(".sidenav-button").click(); }
   });
 
-  $(document).on('keydown', function(e) {
-    if (e.which == 27 && useSideSearch) {
-      $("#close-nav").click();
-    }
-  })
+  function addEscKeyListener() {
+    document.addEventListener('keydown', function(e) {
+      if (e.which == 27 && useSideSearch) { 
+        $("#close-nav").click(); 
+      }
+    })
+  }
 
   function slideSideNav() {
-    var mq = window.matchMedia( "(min-width: 500px)" );
+    const mq = window.matchMedia( "(min-width: 500px)" );
     if (mq.matches) {
-      $(".sidenav").css("width", "250px");
-      $("#map").css("marginLeft", "250px");
+      $(".sidenav").velocity({width: "250px"});
+      $("#map").velocity({marginLeft: "250px"});
       $("#sidenav-query").val(querySearch).focus();
       useSideSearch = true;
-    } else {
-      return;
     }
   }
 
   $("#close-nav").on("click", function() {
     $("#search-query").val('').focus();
-    $("#map").css("marginLeft", "0");
-    $(".sidenav").css("width", "0");
+    $("#map").velocity({marginLeft: "0px"});
+    $(".sidenav").velocity({width: "0px"});
     $(".search-container").show();
     $("#search-query").focus();
     useSideSearch = false;
